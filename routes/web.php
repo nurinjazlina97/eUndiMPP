2d703d0271e243b63acdef7837dec8191c0dc8ea
nurinjazlina97 <nurynn.nj@gmail.com>
2021-07-22 22:16:49 +0800

    Route updatedd

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-// Route::get('/admin', [App\Http\Controllers\LogInChoiceController::class, 'index'])->name('admin');
-Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
+Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
 
 Route::get('/vote', [App\Http\Controllers\VoteController::class, 'index'])->name('vote');
+
+Route::get('/voteprogram', [App\Http\Controllers\VoteProgramController::class, 'index'])->name('voteprogram');
+Route::get('/tambahcalonprogram', [App\Http\Controllers\ProgramCandidateController::class, 'create'])->name('tambahcalonprogram');
+
+Route::post('/tambahcalon', [App\Http\Controllers\GeneralCandidateController::class, 'store'])->name('tambahcalon.simpan');
+Route::post('/tambahcalonprogram/simpan', [App\Http\Controllers\ProgramCandidateController::class, 'store'])->name('tambahcalonprogram.simpan');
+
+Route::get('/datastudent', [App\Http\Controllers\DataStudentController::class, 'index'])->name('student.index');
+
+Route::get('/datastudent/delete/{calonumums}', [App\Http\Controllers\DataStudentController::class, 'destroy'])->name('student.delete');
